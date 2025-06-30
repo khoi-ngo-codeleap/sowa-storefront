9b0c6078eb43b5f3215a31c4ac41cdf3b381eaee
Khoi Ngo <khoi.ngo@codeleap.de>
2025-06-30 14:25:34 +0700

    chore: refactor

--- src/features/customer-playground/jotai-approach/edit-contact-sheet/EditCustomerContactSheet.tsx
+++ src/features/customer-playground/jotai-approach/edit-contact-sheet/EditCustomerContactSheet.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Form } from "@/components/ui/form";
 import {
   Sheet,
   SheetContent,
@@ -7,65 +5,12 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import queryClient from "@/configs/queryClient";
-import CustomerContactSection from "@/features/customer/components/form/CustomerContactSection";
-import {
-  customerContactSchema,
-  CustomerContactValue,
-} from "@/features/customer/components/form/schema";
-import { updateCustomer } from "@/features/customer/domain/command/updateCustomer";
+import CustomerContactForm from "@/features/customer/components/form/CustomerContactForm";
 import { useModal, useModalState } from "@/features/customer/state/modal";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
-import { Loader } from "lucide-react";
-import { SubmitHandler, useForm } from "react-hook-form";
 
 const EditCustomerContactSheet = () => {
   const { isOpen, close, onChange } = useModal("editContact");
   const draftCustomer = useModalState("editContact");
-
-  console.log("draftCustomer ", draftCustomer);
-
-  const { mutate, isPending } = useMutation({
-    meta: {
-      successMessage: "Customer contact updated successfully",
-    },
-    mutationFn: updateCustomer,
-    onSuccess: () => {
-      return queryClient.invalidateQueries({
-        queryKey: ["xxx-customers"],
-      });
-    },
-  });
-
-  const form = useForm<CustomerContactValue>({
-    defaultValues: draftCustomer
-      ? {
-          first_name: draftCustomer.firstName,
-          last_name: draftCustomer.lastName,
-          locale: draftCustomer.locale,
-          email: draftCustomer.email,
-          phone: draftCustomer.phone,
-        }
-      : undefined,
-    resolver: zodResolver(customerContactSchema),
-  });
-
-  const onSubmit: SubmitHandler<CustomerContactValue> = (values) => {
-    if (draftCustomer) {
-      console.log(values);
-      mutate(
-        {
-          id: draftCustomer.id,
-          updateSet: {
-            display_name: `${values.first_name} ${values.last_name}`,
-            ...values,
-          },
-        },
-        { onSuccess: close }
-      );
-    }
-  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onChange}>
@@ -77,22 +22,11 @@ const EditCustomerContactSheet = () => {
           </SheetDescription>
         </SheetHeader>
         <div className="px-4">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <CustomerContactSection />
-              <div className="flex justify-end gap-2">
-                <Button variant="outline" onClick={close} disabled={isPending}>
-                  Cancel
-                </Button>
-                <Button disabled={isPending}>
-                  {isPending && (
-                    <Loader className="mr-2 h-4 w-4 animate-spin" />
-                  )}
-                  Save
-                </Button>
-              </div>
-            </form>
-          </Form>
+          <CustomerContactForm
+            customer={draftCustomer}
+            onCompleted={close}
+            featureFlag="old"
+          />
         </div>
       </SheetContent>
     </Sheet>
--- src/features/customer-playground/page.tsx
+++ src/features/customer-playground/page.tsx
@@ -1,12 +1,9 @@
 import EditCustomerContactSheetTanstack from "../customer-playground/tanstack-approach/edit-contact-sheet/EditCustomerContactSheetTanstack";
-import { useModal } from "../customer/state/modal";
 import EditCustomerContactSheet from "./jotai-approach/edit-contact-sheet/EditCustomerContactSheet";
 import { CustomerList as CustomerListJotai } from "./jotai-approach/pages/CustomerPlaygroundIndex";
 import { CustomerNewList } from "./tanstack-approach/pages/CustomerPlaygroundIndex";
 
 const CustomerPlaygroundIndex = () => {
-  console.log("render: CustomerPlaygroundIndex");
-  const { isOpen } = useModal("editContact");
   return (
     <>
       <div className="flex gap-4">
@@ -14,7 +11,7 @@ const CustomerPlaygroundIndex = () => {
           <h2 className="font-semibold text-xl">Current approach</h2>
           <div>
             <CustomerListJotai />
-            {isOpen && <EditCustomerContactSheet />}
+            <EditCustomerContactSheet />
           </div>
         </div>
 
